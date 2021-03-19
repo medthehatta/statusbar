e09410914bdb44ddd658e51c6c121b0da909b968
Med Mahmoud <medthehatta@gmail.com>
2021-03-19 01:36:58 -0400

    Add statusbar wrapper script

---
+++ statusbar.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+prefix="$(dirname -- "$(readlink -f "$0")")"
+
+
+_cleanup () {
+    kill "$(cat "$XDG_RUNTIME_DIR/wm/dlog.pid")"
+    kill "$(cat "$XDG_RUNTIME_DIR/wm/status.pid")"
+}
+
+
+trap _cleanup EXIT
+
+
+connected_screens="$(xrandr | grep -P '\bconnected\b' | wc -l)"
+
+
+if [ "$connected_screens" == 1 ]; then
+    dlog_pos="$(on_screen <<< 'screen0 50x0 top_left')"
+    status_pos="$(on_screen <<< 'screen0 50x0 top_right')"
+elif [ "$connected_screens" == 2 ]; then
+    dlog_pos="$(on_screen <<< 'screen0 100x0 top_left')"
+    status_pos="$(on_screen <<< 'screen1 100x0 top_right')"
+else
+    # Put it all on one screen; we don't know what's going on with this many
+    # screens
+    dlog_pos="$(on_screen <<< 'screen0 50x0 top_left')"
+    status_pos="$(on_screen <<< 'screen0 50x0 top_right')"
+fi
+
+
+tail -f "$XDG_RUNTIME_DIR/wm/dynamiclog" | dzen2 -p -ta l -dock $dlog_pos &
+echo "$!" > "$XDG_RUNTIME_DIR/wm/dlog.pid"
+
+python3 "$prefix/statusbar.py" | dzen2 -p -ta r -dock $status_pos &
+echo "$!" > "$XDG_RUNTIME_DIR/wm/status.pid"
+
+
+wait
